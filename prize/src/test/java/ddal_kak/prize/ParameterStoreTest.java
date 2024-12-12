135bc40f88107370ad8d6e91f371f95cee9e6055
김장군 <rns0721@MacBook-Pro.local>
2024-12-12 19:22:34 +0900

    test: AWS Parameter Store에서 값을 가져올 수 있다
    - 테스트용 파라미터를 가져와 일치하는지 확인

---
+++ prize/src/test/java/ddal_kak/prize/ParameterStoreTest.java
@@ -0,0 +1,20 @@
+package ddal_kak.prize;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ParameterStoreTest {
+    
+    @Value("${test}")
+    String value;
+    
+    @Test
+    void 파라미터스토어에서_값을_가져올_수_있다() {
+        System.out.println("value = " + value);
+        Assertions.assertThat(value).isEqualTo("test value");
+    }
+    
+}
