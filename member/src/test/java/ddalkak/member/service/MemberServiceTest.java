41b40b76c681420368288546845d7602fc1b295b
woo <ldw28517@naver.com>
2025-04-17 03:13:58 +0900

    test: 회원가입 테스트
    - 서비스 레이어 테스트
    - 중복 이메일 처리로 커스텀 예외 터지는지 확인
    - 정상 처리 확인

---
+++ member/src/test/java/ddalkak/member/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package ddalkak.member.service;
+
+import ddalkak.member.domain.Member;
+import ddalkak.member.dto.request.SignUpRequest;
+import ddalkak.member.common.exception.DuplicatedEmailException;
+import ddalkak.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private MemberService memberService;
+
+    @Test
+    void 회원가입_성공() {
+        //given
+        SignUpRequest request = new SignUpRequest("홍길동", "test@email.com", "password");
+        Member expected = Member.createGeneralMember(request.name(), request.email(), request.password());
+        when(memberRepository.findByEmail(request.email()))
+                .thenReturn(Optional.empty());
+        when(memberRepository.save(any()))
+                .thenReturn(expected);
+        when(passwordEncoder.encode(request.password()))
+                .thenReturn(request.password());
+
+        //when
+        Member savedMember = memberService.signup(request);
+
+        //then
+        Assertions.assertThat(memberIsSame(expected, savedMember)).isTrue();
+    }
+
+    @Test
+    void 회원가입_실패_중복된_이메일() {
+        //given
+        SignUpRequest request = new SignUpRequest("홍길동", "test@email.com", "password");
+        when(memberRepository.findByEmail(request.email()))
+                .thenReturn(Optional.of(new Member()));
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> memberService.signup(request))
+                .isInstanceOf(DuplicatedEmailException.class);
+    }
+
+    private boolean memberIsSame(Member mem1, Member mem2) {
+        return mem1.getName() == mem2.getName() &&
+                mem1.getEmail() == mem2.getEmail() &&
+                mem1.getPassword() == mem2.getPassword();
+    }
+}
