826956ea22d7f286ff182e909cf95d5f25bf9ded
woo <ldw28517@naver.com>
2025-04-23 17:34:51 +0900

    refactor(LoginResponse): 정적 팩토리 메서드 추가
    - LoginResponse 생성에 대한 캡슐화 + 로그인 성공 핸들러 간소화를 위해 사용

--- member/src/main/java/ddalkak/member/dto/response/LoginResponse.java
+++ member/src/main/java/ddalkak/member/dto/response/LoginResponse.java
@@ -1,9 +1,18 @@
 package ddalkak.member.dto.response;
 
+import ddalkak.member.domain.entity.Member;
+import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public record LoginResponse(Long memberId,
                             String name,
                             String email) {
+    public static LoginResponse from(Member member) {
+        return LoginResponse.builder()
+                .memberId(member.getMemberId())
+                .name(member.getName())
+                .email(member.getEmail())
+                .build();
+    }
 }
