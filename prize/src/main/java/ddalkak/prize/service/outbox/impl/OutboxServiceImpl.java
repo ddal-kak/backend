1907893edd149044a067e6902166347a82fd0e2d
qnmgjeklnbrl <rns0721@naver.com>
2025-05-07 17:20:46 +0900

    feat: 재고감소시 동시성 처리 및 outbox테이블에 결과저장
    - 동시에 재고감소 요청이 들어오면 재시도
    - 5번 까지 재시도후 실패시 예외 발생
    - outbox테이블에 결과 저장

---
+++ prize/src/main/java/ddalkak/prize/service/outbox/impl/OutboxServiceImpl.java
@@ -0,0 +1,39 @@
+package ddalkak.prize.service.outbox.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ddalkak.prize.domain.entity.Outbox;
+import ddalkak.prize.dto.DecreaseResultEvent;
+import ddalkak.prize.dto.DecreaseStockEvent;
+import ddalkak.prize.repository.outbox.OutboxRepository;
+import ddalkak.prize.service.outbox.OutBoxService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class OutboxServiceImpl implements OutBoxService {
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
+    @Override
+    public Long save(DecreaseStockEvent event, boolean result) {
+
+        DecreaseResultEvent decreaseResultEvent = new DecreaseResultEvent(
+                event.eventId(),
+                event.prizeId(),
+                result
+        );
+        String payload = null;
+        try {
+            payload = objectMapper.writeValueAsString(decreaseResultEvent);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while converting event to JSON");
+        }
+       Outbox savedOutbox = outboxRepository.save(new Outbox(event.eventId(), payload));
+
+        return savedOutbox.getId();
+
+
+    }
+}
