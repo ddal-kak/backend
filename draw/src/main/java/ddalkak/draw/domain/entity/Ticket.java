e0c7d9f498d5ca36a4e65302b33958218a3224f4
woo <ldw28517@naver.com>
2025-07-01 23:34:33 +0900

    feat: 회원가입 이벤트 수신 후 유저 티켓을 초기화
    - Ticket.java: 티켓 생성 용 정적 팩터리 메서드 추가 및 memberId에 유니크 제약 조건 추가(중복 메세지 처리 시 멱등성 보장)
    - SignUpEventListener.java: 이벤트 수신 지점, 새로 생성한 회원가입 이벤트 처리용 컨슈머 팩토리 활용
    - TicketService.java: save()용 메서드 추가

--- draw/src/main/java/ddalkak/draw/domain/entity/Ticket.java
+++ draw/src/main/java/ddalkak/draw/domain/entity/Ticket.java
@@ -13,11 +13,21 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private Long memberId;
     private int quantity;
+    // 더블 클릭 등으로 인한 응모권 사용 동시성 제어
+    // 로그인 중복 메세지 발행으로 인한 동시성 이슈를 제어
     @Version
-    private Long version;// 더블 클릭 등으로 인한 응모권 사용 동시성 제어
+    private Long version;
     private LocalDate lastLogin;
+
+    public static Ticket of(long memberId) {
+        return Ticket.builder()
+                .memberId(memberId)
+                .quantity(0)
+                .build();
+    }
 
     public void decrease() {
         if (quantity <= 0) {
