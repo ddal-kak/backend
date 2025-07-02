bb4e572b7b030ebbf26e7e7109c80b2a1198b1bd
woo <ldw28517@naver.com>
2025-07-02 22:59:52 +0900

    feat: Prize 서버로부터 받는 PrizeDto 정보 수정
    - 기존에는 확률만 가지고 왔는데 Prize Name이 필요해 추가

--- draw/src/main/java/ddalkak/draw/domain/Prize.java
+++ draw/src/main/java/ddalkak/draw/domain/Prize.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DrawProbability(@JsonProperty("probabilityRange") Long range,
-                              @JsonProperty("randomNumber") Long winNumber) {
+public record Prize(@JsonProperty("name") String name,
+                    @JsonProperty("probabilityRange") Long range,
+                    @JsonProperty("randomNumber") Long winNumber) {
 }
