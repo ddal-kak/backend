05e09ab1b827a589b7704956bba924972f97bcd0
woo <ldw28517@naver.com>
2025-07-03 21:35:27 +0900

    feat: Base Auditing Entity 추가
    - 생성 및 수정 시각 추적

--- draw/src/main/java/ddalkak/draw/domain/entity/Ticket.java
+++ draw/src/main/java/ddalkak/draw/domain/entity/Ticket.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 
 @Getter
 @Entity
-public class Ticket {
+public class Ticket extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
