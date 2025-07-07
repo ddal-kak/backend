516ca168864e4c7ee45f1b65d676cf6ef29138d1
woo <ldw28517@naver.com>
2025-07-08 01:32:31 +0900

    feat: Global Exception Handler 구현
    - Business Exception, 이 외 예상치 못한 예외를 처리하는 예외 핸들러 구현
    - 기본 Spring MVC 관련 예외는 ResponseEntityExceptionHandler를 상속하여 처리, 변경 가능

--- draw/src/main/java/ddalkak/draw/common/exception/ErrorCode.java
+++ draw/src/main/java/ddalkak/draw/common/exception/ErrorCode.java
@@ -1,11 +1,14 @@
 package ddalkak.draw.common.exception;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    INSUFFICIENT_TICKET(HttpStatus.FORBIDDEN, "응모권이 부족합니다.");
+    INSUFFICIENT_TICKET(HttpStatus.FORBIDDEN, "응모권이 부족합니다."),
+    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생했습니다.");
 
     private final HttpStatus status;
     private final String message;
