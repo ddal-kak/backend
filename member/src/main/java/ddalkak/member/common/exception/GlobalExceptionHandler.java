236f6a4ce4cb4c27c074aeeaa6479315c5f43979
woo <ldw28517@naver.com>
2025-07-15 02:26:07 +0900

    feat: Input Validation 실패 핸들링
    - 기존에 ResponseEntityExceptionHandler를 상속받아 Spring MVC 공통 예외를 처리했지만, 응답 디테일이 부족하여 제거
    - 로그인 및 회원가입 시도 시 각 필드에 대한 검증 오류를 반환하도록 추가

--- member/src/main/java/ddalkak/member/common/exception/GlobalExceptionHandler.java
+++ member/src/main/java/ddalkak/member/common/exception/GlobalExceptionHandler.java
@@ -4,17 +4,27 @@ import ddalkak.member.common.exception.errorcode.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler { // Spring MVC Exception은 ResponseEntityExceptionHandler에 의해 기본 처리
+public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         return makeErrorResponse(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        String errorMessage = fieldError.getField() + ": " + fieldError.getDefaultMessage();
+        return makeErrorResponse(HttpStatus.BAD_REQUEST, "BAD_REQUEST", errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
@@ -29,6 +39,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler { // 
                 .body(ErrorResponse.builder()
                         .errorCode(errorCode.name())
                         .message(errorCode.getMessage())
+                        .build());
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponse(HttpStatus status, String errorCode, String errorMessage) {
+        return ResponseEntity
+                .status(status)
+                .body(ErrorResponse.builder()
+                        .errorCode(errorCode)
+                        .message(errorMessage)
                         .build());
     }
 
