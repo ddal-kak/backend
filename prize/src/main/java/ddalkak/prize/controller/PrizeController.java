dc34ad1c20464c379f17717bd224b76dc7f0e4a2
qnmgjeklnbrl <rns0721@naver.com>
2025-08-09 23:16:37 +0900

    chore: api 경로 수정

--- prize/src/main/java/ddalkak/prize/controller/PrizeController.java
+++ prize/src/main/java/ddalkak/prize/controller/PrizeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/prizes")
+@RequestMapping("/api/prizes")
 @RequiredArgsConstructor
 @Validated
 public class PrizeController {
