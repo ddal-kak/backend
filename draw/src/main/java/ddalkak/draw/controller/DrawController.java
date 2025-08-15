c9cfb1a2dc7e2a837727c5c8b1e34db647979155
woo <ldw28517@naver.com>
2025-08-15 15:37:46 +0900

    chore: api path 수정

--- draw/src/main/java/ddalkak/draw/controller/DrawController.java
+++ draw/src/main/java/ddalkak/draw/controller/DrawController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/draws")
+@RequestMapping("/api/draws")
 @RequiredArgsConstructor
 public class DrawController {
     private final DrawService drawService;
