90feb76af23efaa216bd7af5b4020bd88def081d
woo <ldw28517@naver.com>
2025-09-10 00:07:50 +0900

    feat: 멱등성을 가지는 이벤트 처리 핸들러 구현
    - 핸들러에서 부모 트랜잭션을 시작, 비지니스 로직 및 멱등성 관련 이벤트 정보 처리를 하나의 트랜잭션으로 묶음
    - 리턴되어(트랜잭션에 성공하여) KafkaConsumer 측으로 복귀하면 ack 처리, 트랜잭션 commit 후 ack처리를 하기 위한 구조

---
+++ draw/src/main/java/ddalkak/draw/service/event/ExternalEventHandler.java
@@ -0,0 +1,42 @@
+package ddalkak.draw.service.event;
+
+import ddalkak.draw.common.aop.annotation.EnableIdempotent;
+import ddalkak.draw.domain.DrawResult;
+import ddalkak.draw.dto.event.DecreaseResult;
+import ddalkak.draw.dto.event.DecreaseStockEvent;
+import ddalkak.draw.dto.event.LoginEvent;
+import ddalkak.draw.dto.event.SignUpEvent;
+import ddalkak.draw.service.core.DrawService;
+import ddalkak.draw.service.ticket.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ExternalEventHandler {
+    private final TicketService ticketService;
+    private final DrawService drawService;
+
+    @Transactional
+    @EnableIdempotent(eventId = "#event.eventId()")
+    public void handleLoginEvent(LoginEvent event) {
+        ticketService.rewardDailyLogin(event.memberId(), event.occurAt());
+    }
+
+    @Transactional
+    @EnableIdempotent(eventId = "#event.eventId()")
+    public void handleSignupEvent(SignUpEvent event) {
+        ticketService.initTicket(event.memberId());
+    }
+
+    @Transactional
+    @EnableIdempotent(eventId = "#event.eventId()")
+    public void handleDecreaseStockEvent(final DecreaseStockEvent event) {
+        if (event.result() == DecreaseResult.SUCCESS) {
+            drawService.finalizeDrawResult(event.drawId(), DrawResult.WIN);
+        } else {
+            drawService.finalizeDrawResult(event.drawId(), DrawResult.LOSE);
+        }
+    }
+}
