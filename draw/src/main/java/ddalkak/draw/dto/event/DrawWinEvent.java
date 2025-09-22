c28f825cc42bc2648b63e591d8f5a216d8884f66
woo <ldw28517@naver.com>
2025-09-22 20:59:55 +0900

    feat: 경품 당첨 이벤트 필드 추가
    - memberId 추가
    - 재고 차감 결과 이벤트에도 동일 필드 추가

--- draw/src/main/java/ddalkak/draw/dto/event/DrawWinEvent.java
+++ draw/src/main/java/ddalkak/draw/dto/event/DrawWinEvent.java
@@ -8,12 +8,14 @@ import java.time.Instant;
 @Builder(access = AccessLevel.PRIVATE)
 public record DrawWinEvent(long eventId,
                            long drawId,
+                           long memberId,
                            long prizeId,
                            Instant occurAt) implements ExternalEvent {
-    public static DrawWinEvent of(long eventId, long drawId, long prizeId) {
+    public static DrawWinEvent of(long eventId, long drawId, long memberId, long prizeId) {
         return DrawWinEvent.builder()
                 .eventId(eventId)
                 .drawId(drawId)
+                .memberId(memberId)
                 .prizeId(prizeId)
                 .occurAt(Instant.now())
                 .build();
