583ac43c146c06695fca8659bbb960bc6dc05969
woo <ldw28517@naver.com>
2025-11-08 23:31:03 +0900

    refactor: JpaAuditing config 파일로 분리

--- member/src/main/java/ddalkak/member/MemberApplication.java
+++ member/src/main/java/ddalkak/member/MemberApplication.java
@@ -2,14 +2,12 @@ package ddalkak.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class MemberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
 
 }
