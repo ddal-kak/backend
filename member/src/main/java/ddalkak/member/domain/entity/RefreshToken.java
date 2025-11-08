b719ec9aecbe63608d45a703f000c1478ef3b775
woo <ldw28517@naver.com>
2025-11-08 21:14:14 +0900

    refactor(RefreshToken.class): 필드 간 줄바꿈 추가

--- member/src/main/java/ddalkak/member/domain/entity/RefreshToken.java
+++ member/src/main/java/ddalkak/member/domain/entity/RefreshToken.java
@@ -10,14 +10,15 @@ public class RefreshToken extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tokenId;
+
     @OneToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
     @Column(length = 1000)
     private String token;
 
     public RefreshToken() {
-
     }
 
     @Builder
